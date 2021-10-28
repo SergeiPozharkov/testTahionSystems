a972f1d809def23652bbb51b9c52c863407f8763
SergeiPozharkov <pozharkov.sergei@mail.ru>
2021-10-28 19:52:25 +0300

    update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,3 @@
-// const deleteBtn = document.getElementById('deleted');
-//
-// deleteBtn.addEventListener('click', confirmCheck);
-
 function confirmCheck(atr) {
     const modal = confirm('Вы действительно хотите удалить данного пользователя?')
     if (modal === false) {
